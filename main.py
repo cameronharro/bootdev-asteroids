d4c7c9867a50b4bbd7484db54248be0a71f26424
cameronharro <103271364+cameronharro@users.noreply.github.com>
2025-04-08 15:56:49 -0500

    perf: add calculations to limit game loop to 60 fps

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
   print("Starting Asteroids!")
   print(f"Screen width: {SCREEN_WIDTH}")
   print(f"Screen height: {SCREEN_HEIGHT}")
+
   pygame.init()
+  clock = pygame.time.Clock()
+  dt = 0
   display = pygame.display
   screen = display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+  # describe game loop
   while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
     screen.fill("black")
     display.flip()
+
+    # limit loop to 60 fps
+    dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
   main()
