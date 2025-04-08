05ec81af0f6789e3516d402f7929e217f699a393
cameronharro <103271364+cameronharro@users.noreply.github.com>
2025-04-08 16:37:17 -0500

    refactor: Add player class to groups for convenient update/draw calls

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ def main():
   dt = 0
   display = pygame.display
   screen = display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+  updateable = pygame.sprite.Group()
+  drawable = pygame.sprite.Group()
+  Player.containers = (updateable, drawable)
   player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
   # describe game loop
@@ -19,9 +23,10 @@ def main():
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
-    player.update(dt)
+    updateable.update(dt)
     screen.fill("black")
-    player.draw(screen)
+    for item in drawable:
+      item.draw(screen)
     display.flip()
 
     # limit loop to 60 fps
