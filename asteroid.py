9daba9e7f2e8f41f876b690bdf949103156a65a8
cameronharro <103271364+cameronharro@users.noreply.github.com>
2025-04-08 21:43:11 -0500

    refactor: encapsulate velocity in Asteroid constructor

--- asteroid.py
+++ asteroid.py
@@ -6,8 +6,9 @@ from constants import (
 )
 
 class Asteroid(CircleShape):
-  def __init__(self, x, y, radius):
+  def __init__(self, x, y, radius, velocity):
     super().__init__(x, y, radius)
+    self.velocity = velocity
   
   def draw(self, screen):
     pygame.draw.circle(screen, "white", self.position, self.radius, 2)
